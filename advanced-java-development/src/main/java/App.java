dd9ae35da6b0e52b49ff9aad38f03933c2b9ca10
John Grove <dev.grovejt@gmail.com>
2021-04-07 16:22:33 -0500

    added course files

---
+++ advanced-java-development/src/main/java/App.java
@@ -0,0 +1,8 @@
+import generics.Pair;
+
+public class App {
+    public static void main(String[] args) {
+        Pair<String> p1 = new Pair("s1", "s2");
+        System.out.printf("Pair: %s", p1);
+    }
+}
---
+++ advanced-java-development/src/main/java/generics/Pair.java
@@ -0,0 +1,17 @@
+package generics;
+
+import lombok.Data;
+
+@Data
+public class Pair<T> {
+
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    pu
+}
